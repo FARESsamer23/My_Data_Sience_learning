e076672568808b03de9aa009c2b93709fb81fdff
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-11-08 14:22:44 -1200

    #149

---
+++ Learn_Numpy/Array_shape_and_reshpe.py
@@ -0,0 +1,34 @@
+import numpy  as np
+
+# my_array =  np.array([[1,2,3,4],[5,6,7,8]])
+# print(my_array.ndim)
+# print(my_array.shape) # (rows ,coloms) 2d array but in 1d array (coloms)
+# print(my_array.reshape())
+# (chhal 3andek min array f dimension ta3ek,arrays ta3ek chhal fihom mon elemnet )
+my_array =  np.array([
+                      [[1,2,3,4,1],[5,6,7,8,1]],
+                      [[11,12,13,14,1],[15,16,17,18,1]],
+                     ])
+
+# print(my_array.ndim)
+# print(my_array.shape) 
+# print(my_array)
+
+print('#'*50)
+
+print( my_array.reshape(5,4)) # haka rahi 2d
+print(my_array.reshape(2,5,2)) # haka rahi 3d
+print(my_array.ndim)
+
+
+# for x in my_array :
+#     for n in x:
+#         for f in n:
+#             print(f)
+
+
+'''
+reshape(-1) :  tronsferm the array  to 1 dimension
+ 
+'''
+       
